4f86f59b1ee8710ba82a92af143b4325c8a965cf
KarenYuanMei <ym2@alum.mit.edu>
2021-02-10 14:59:17 -0800

    add function changes

--- run_train_crossvalidate_pipeline.py
+++ run_train_crossvalidate_pipeline.py
@@ -14,7 +14,7 @@ from itertools import combinations, combinations_with_replacement
 from itertools import product
 from collections import defaultdict
 
-from define_gene_objects import define_features, Gene, PairOfGenes, find_input_features, load_feature, create_feature_value_dict, get_feature_value, create_GO_score_dict, create_gene_list, find_pos_genes_in_training, find_gene_objects, find_feature_array, create_input_pair_objects, run_adaboost, run_svm_regressor, run_random_forest, find_new_genes, create_new_gene_list, run_new_rf, find_new_array, find_avg_scores
+from define_gene_objects import define_features, Gene, PairOfGenes, find_input_features, load_feature, create_feature_value_dict, get_feature_value, create_GO_score_dict, create_gene_list, find_pos_genes_in_training, find_gene_objects, find_feature_array, create_input_pair_objects, run_adaboost, run_svm_regressor, run_random_forest, find_new_genes, run_new_rf, find_new_array, find_avg_scores
 from load_data_functions import get_gene_names
 from find_training_genes_scores_functions import make_genes_csv, make_mat_csv, random_select, find_pos_neg_input, divide_5fold, find_pos_neg_chunks, define_training_test, find_GO_ont, find_GO_score_matrix, find_input_gene_GO_scores
 
