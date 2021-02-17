04025179725e96cfb0cd5280cdcc5d1a2d1e46c6
KarenYuanMei <ym2@alum.mit.edu>
2021-02-17 13:34:42 -0800

    update function

--- ML_functions/find_training_genes_functions.py
+++ ML_functions/find_training_genes_functions.py
@@ -105,7 +105,7 @@ def load_big_pool():
 
 def define_crossvalidation_genes(pos, pos_chunks, neg, neg_chunks):
 	for i in range(5):
-		training_gene_names, test_gene_names=find_training_genes_functions.define_training_test(pos, pos_chunks, neg, neg_chunks, i)
+		training_gene_names, test_gene_names=define_training_test(pos, pos_chunks, neg, neg_chunks, i)
 		training_df=find_training_genes_functions.make_genes_csv(training_gene_names, 'updated', 'training_genes_%s'%i)
 		test_df=find_training_genes_functions.make_genes_csv(test_gene_names, 'updated', 'test_genes_%s'%i)
 	return training_gene_names, test_gene_names
