185417f06993dc6e619832d66b41b436b8c05aa3
KarenYuanMei <ym2@alum.mit.edu>
2021-02-18 13:27:00 -0800

    update functions

--- run_ML/calculate_cv_ROC.py
+++ run_ML/calculate_cv_ROC.py
@@ -43,6 +43,7 @@ def make_score_table(data, training_positives):
 	idx = table.index[table.isnull().all(1)]
 	table=table[training_positives]
 	table['mean']=table.mean(axis=1)
+	print (table)
 	return table
 
 def find_group_df(table, genelist, test, label):
@@ -56,6 +57,7 @@ def make_input_ROC_df(data, training_positives, pos, neg, test):
 	pos_df=find_group_df(table, pos, test, 1)
 	neg_df=find_group_df(table, neg, test, 0)
 	input_df=pd.concat([pos_df, neg_df], axis=1)
+	print ('input', input_table)
 	avg_score=input_df['mean'].tolist()
 	label=input_df['group'].tolist()
 	return label, avg_score
