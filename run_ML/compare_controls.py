195299f65247e1de30af274f2cb1c8d4e807f9d2
KarenYuanMei <ym2@alum.mit.edu>
2021-02-22 17:20:33 -0800

    add controls

--- run_ML/compare_controls.py
+++ run_ML/compare_controls.py
@@ -71,7 +71,7 @@ big_pool=find_training_genes_functions.load_big_pool()
 
 pos, neg, all_training=find_training_genes_functions.load_pos_neg_training()
 
-human_ont=find_training_genes_functions.find_GO_ont()
+human_ont=find_GO_scores.find_GO_ont()
 go_genes=human.ont.genes
 
 hk=find_hk(big_pool)
