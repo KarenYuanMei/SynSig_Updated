95a82e0c2ebb87d38666a532cf8e2c0c79f30a19
KarenYuanMei <ym2@alum.mit.edu>
2021-03-05 14:11:25 -0800

    add function

--- network_analysis/run_rwalk/run_net_rwalk.py
+++ network_analysis/run_rwalk/run_net_rwalk.py
@@ -1,4 +1,4 @@
-#Goal: use the environment net-pop to run random walk
+#Goal: use the environment jh_network to run random walk
 
 import scipy as sp
 import networkx as nx
@@ -10,13 +10,18 @@ import sys
 sys.path.append('../rwalk_functions/')
 import make_network_graph_functions
 
+sys.path.append('../../read_data_functions')
+import load_data_functions
+
+
 sys.path.append('../ppi_files/')
 
 filename='../ppi_files/Human_Mentha_converted.csv'
 df=make_network_graph_functions.make_mentha_df(filename)
 print (df)
 
-#print (nx.test())
-
 G=make_network_graph_functions.make_network_G(df)
 print (G.number_of_edges())
+
+seeds=load_data_functions.get_gene_names('../../run_ML/ML_output/training_genes/updated_positives.csv')
+print (len(seeds))
