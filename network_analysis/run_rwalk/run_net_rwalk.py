785e317d5389ab98ef1c4aecdd7fa15c201d1c0e
KarenYuanMei <ym2@alum.mit.edu>
2021-03-08 15:34:47 -0800

    large prop

--- network_analysis/run_rwalk/run_net_rwalk.py
+++ network_analysis/run_rwalk/run_net_rwalk.py
@@ -152,7 +152,7 @@ ordered_set={'syngo': ordered_test}
 neg=list(set(nodes)-set(syngo))
 
 kernel=net_random_walk_functions.construct_prop_kernel(G, 0.4, verbose=True)
-df=find_prop_scores_df(kernel, nodesets, 0.5)
+df=find_prop_scores_df(kernel, ordered_set, 0.5)
 		#print (df)
 mean_fpr, tprs, aucs=calc_prop_aucs(df, neg)
 print (aucs)
