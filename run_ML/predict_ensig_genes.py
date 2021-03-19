3fd67f04d8003f3354828fa05ab4511109960a5c
KarenYuanMei <ym2@alum.mit.edu>
2021-03-19 16:25:46 -0700

    find common index

--- run_ML/predict_ensig_genes.py
+++ run_ML/predict_ensig_genes.py
@@ -24,3 +24,40 @@ import define_features
 nonbrain_features=define_features.load_nonbrain_features()
 
 print (nonbrain_features)
+
+def find_common_pool(nonbrain_features):
+	string_files=['pFAM_domain', 'mentha_source_feature','biogrid_source_feature', 'bioplex_source_feature', 'chr_no_source_feature']
+	kernel_file=['mentha_kernel']
+	gtex_rna_file=['gtex_rna_kernel']
+	bioplex_file=['bioplex_kernel']
+	if filename in string_files:
+		df = pd.read_csv('../../../SynSig/features/normalized_%s.csv'%filename,converters={"Interactors": lambda x: x.strip("[]").split(", ")})
+		symbol=df['Norm_Symbol']
+		df.drop(labels=['Norm_Symbol', 'Genes'], axis=1,inplace = True)
+		df.insert(0, 'Genes', symbol)
+		df=df.set_index('Genes')
+		idx=list(df.index)
+
+	elif filename in kernel_file:
+		df=pd.read_csv('../../../../Network_propagation/propagate_synapse/mentha_kernel.csv', index_col=[0])
+		idx=list(df.index)
+
+	elif filename in bioplex_file:
+		df=pd.read_csv('../network_analysis/run_rwalk/bioplex_kernel.csv', index_col=[0])
+		idx=list(df.index)
+
+	else:
+		df=pd.read_csv('../../../SynSig/features/normalized_%s.csv'%filename)
+
+		symbol=df['Norm_Symbol']
+		df.drop(labels=['Norm_Symbol', 'Genes'], axis=1,inplace = True)
+		df.insert(0, 'Genes', symbol)
+		#print (df)
+		df=df.set_index('Genes')
+		idx=list(df.index)
+		
+	return idx
+
+for feature in nonbrain_features:
+	idx=find_common_pool(nonbrain_features)
+	print (idx[:5])
