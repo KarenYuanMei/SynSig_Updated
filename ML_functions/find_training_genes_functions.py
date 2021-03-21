b5e636a5e43c4a6ed4988cb9fa6268a8a83dcf85
KarenYuanMei <ym2@alum.mit.edu>
2021-03-21 13:16:09 -0700

    save file path

--- ML_functions/find_training_genes_functions.py
+++ ML_functions/find_training_genes_functions.py
@@ -39,7 +39,7 @@ def find_pos_neg_input(syngo, big_pool, GO_genes):
 
 def make_genes_csv(genelist, first_word, df_name):
 	df=pd.DataFrame({'genes': genelist})
-	df.to_csv('%s_%s.csv'%(first_word, df_name))
+	df.to_csv('../run_ML/ML_output/%s_%s.csv'%(first_word, df_name))
 	return df
 
 #divide list of genes into five chunks for 5-fold cross-validation
