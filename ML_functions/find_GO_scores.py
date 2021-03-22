df198f589b404b7555db6aef81c4348cfc50c7b7
KarenYuanMei <ym2@alum.mit.edu>
2021-03-22 13:44:06 -0700

    find go scores

--- ML_functions/find_GO_scores.py
+++ ML_functions/find_GO_scores.py
@@ -38,12 +38,12 @@ def find_input_gene_GO_scores(positive_genes, negative_genes, go_human):
 	return GO_score_matrix
 
 def make_mat_csv(mat, name):
-	matrix.to_csv('%s_GO_training_score_matrix_for_big_pool_genes.csv'%name)
+	matrix.to_csv('../run_ML/ML_output/training_genes/%s_GO_training_score_matrix_for_big_pool_genes.csv'%name)
 	return matrix
 
 def define_GO_score_matrix(pos, neg, GO_human, name):
 	GO_score_matrix=find_input_gene_GO_scores(pos, neg, GO_human)
-	make_mat_csv(GO_score_matrix, '../run_ML/ML_output/training_genes/%s_GO_training_score_matrix_for_big_pool_genes.csv'%name)
+	make_mat_csv(GO_score_matrix, name)
 	return GO_score_matrix
 
 def load_GO_score_matrix(go_mat_filename):
