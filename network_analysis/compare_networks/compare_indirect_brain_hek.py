3b8a9816c340b7afc228baaff432e98b01b66d01
KarenYuanMei <ym2@alum.mit.edu>
2021-04-01 15:31:50 -0700

    find avg prop edge wts

--- network_analysis/compare_networks/compare_indirect_brain_hek.py
+++ network_analysis/compare_networks/compare_indirect_brain_hek.py
@@ -55,7 +55,12 @@ def find_avg_wts(kernel):
 		avg=mean([weight1, weight2])
 		#print (avg)
 		avg_wts.append(avg)
-	return avg_wts
+	return gene_pairs, avg_wts
+
+def find_prop_edges_wt_df(gene_pairs, avg_wts, name):
+	df=pd.DataFrame({'edges': gene_pairs, 'weights': avg_wts})
+	df.to_csv('%s_prop_edge_wts.csv')
+	return df
 
 if __name__=="__main__":
 
@@ -67,13 +72,15 @@ if __name__=="__main__":
 	bio_fil=run_net_rwalk.make_filtered_bioplex()
 	print (bio_fil)
 	bio_kernel=net_random_walk_functions.construct_prop_kernel(bio_fil, 0.4, verbose=True)
-	weights=bio_kernel.values.tolist()
-	print (len(weights))
+	gene_pairs, avg_wts=find_avg_wts(bio_kernel)
+	prop_df=find_prop_edges_wt_df(gene_pairs, avg_wts, 'bio_fil')
+
+
+
 
 	
 	#avg_wts=find_avg_wts(kernel)
 
-	make_histogram(weights, 'edge_weights', 'frequency', 'edge weight distr', 'bioplex_kernel_weights')
 
 	# #load brain ppi:
 	# brain_net=make_brain_ppi_functions.make_brain_network()
