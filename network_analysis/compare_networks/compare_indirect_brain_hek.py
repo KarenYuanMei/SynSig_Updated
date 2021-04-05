bc2f488bde6abae478cdbcd1fe1a565567353f7d
KarenYuanMei <ym2@alum.mit.edu>
2021-04-05 11:43:03 -0700

    simplify compare indirect edges

--- network_analysis/compare_networks/compare_indirect_brain_hek.py
+++ network_analysis/compare_networks/compare_indirect_brain_hek.py
@@ -59,7 +59,8 @@ def find_avg_wts(common_nodes, kernel):
 		avg_wts.append(avg)
 	return gene_pairs, avg_wts
 
-def find_prop_edges_wt_df(gene_pairs, avg_wts, name):
+def find_prop_edges_wt_df(common_nodes, kernel, name):
+	gene_pairs, avg_wts=find_avg_wts(common_nodes, kernel)
 	node1=[x[0] for x in gene_pairs]
 	node2=[x[1] for x in gene_pairs]
 
@@ -79,45 +80,29 @@ def threshold_df(df, threshold):
 
 	new=df[df['weights']>p]
 	return new
+
 if __name__=="__main__":
 
-	# #load the big gene pool for main random forest:
-	# big_pool=load_data_functions.load_big_pool()
-	# print (len(big_pool))
+	#load the bioplex ppi:
+	bio_fil=run_net_rwalk.make_filtered_bioplex()
+	#print (bio_fil)
+	bio_kernel=net_random_walk_functions.construct_prop_kernel(bio_fil, 0.4, verbose=True)
 
-	# #load the bioplex ppi:
-	# bio_fil=run_net_rwalk.make_filtered_bioplex()
-	# #print (bio_fil)
-	# bio_kernel=net_random_walk_functions.construct_prop_kernel(bio_fil, 0.4, verbose=True)
-	# #gene_pairs, avg_wts=find_avg_wts(bio_kernel)
-	# #prop_df=find_prop_edges_wt_df(gene_pairs, avg_wts, 'bio_fil')
+	# #load brain ppi:
+	brain_ppi=pd.read_csv('../../source_data_files/ppi_files/brain_ppi_df.csv')
+	brain_net=make_network_graph_functions.make_network_G(brain_ppi)
+	brain_kernel=net_random_walk_functions.construct_prop_kernel(brain_net, 0.4, verbose=True)
 
-	# #new=bio_kernel.stack().reset_index().rename(columns={'level_0':'Source','level_1':'Target', 0:'Weight'})
-	# #new.to_csv('biofil_prop_edge_wts.csv'
+	common_nodes=list(set(bio_fil.nodes())&set(brain_net.nodes()))
+	print (len(common_nodes))
+	
+	bio_fil=find_prop_edges_wt_df(common_nodes, bio_kernel, 'bio_fil')
+	brain_net=find_prop_edges_wt_df(common_nodes, brain_kernel, 'brain_net')
 
-
-	# # #load brain ppi:
-	# brain_ppi=pd.read_csv('../../source_data_files/ppi_files/brain_ppi_df.csv')
-	# brain_net=make_network_graph_functions.make_network_G(brain_ppi)
-	# brain_kernel=net_random_walk_functions.construct_prop_kernel(brain_net, 0.4, verbose=True)
-	# #brain_prop_df=brain_kernel.stack().reset_index().rename(columns={'level_0':'Source','level_1':'Target', 0:'Weight'})
-	# #brain_prop_df.to_csv('brain_prop_edge_wts.csv')
-
-	# common_nodes=list(set(bio_fil.nodes())&set(brain_net.nodes()))
-	# print (len(common_nodes))
-	# gene_pairs=list(combinations(common_nodes, 2))
-	# print (len(gene_pairs))
-
-	# gene_pairs, avg_wts=find_avg_wts(common_nodes, bio_kernel)
-	# find_prop_edges_wt_df(gene_pairs, avg_wts, 'bio_fil')
-
-	# gene_pairs, avg_wts=find_avg_wts(common_nodes, brain_kernel)
-	# find_prop_edges_wt_df(gene_pairs, avg_wts, 'brain_net')
-
-	bio_fil=load_prop_edges_wt_df('bio_fil')
+	#bio_fil=load_prop_edges_wt_df('bio_fil')
 	#print (bio_fil)
 
-	brain_net=load_prop_edges_wt_df('brain_net')
+	#brain_net=load_prop_edges_wt_df('brain_net')
 	#print (brain_net)
 
 	percentiles=[80, 85, 90, 95, 99]
