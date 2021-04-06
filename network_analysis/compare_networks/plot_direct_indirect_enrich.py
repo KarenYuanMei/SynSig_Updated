2694964222731cafa289a5262b9e77d70a1c80bf
KarenYuanMei <ym2@alum.mit.edu>
2021-04-05 17:40:02 -0700

    plot edge comparison

--- network_analysis/compare_networks/plot_direct_indirect_enrich.py
+++ network_analysis/compare_networks/plot_direct_indirect_enrich.py
@@ -22,7 +22,7 @@ matplotlib.rcParams.update({'font.size': 14})
 def plot_bargraph(labels, mean_values, xlabel, ylabel, name):
 	x_pos=np.arange(len(labels))
 	#plt.bar(labels, mean_values, yerr=sem, color=['#7f6d5f', '#2d7f5e', '#557f2d','silver', 'dimgray', 'rosybrown'], align='center', ecolor='black', capsize=10)
-	plt.bar(labels, mean_values, align='center', ecolor='black', width=0.3, capsize=10)
+	plt.bar(labels, mean_values, align='center', ecolor='black', capsize=10)
 
 	#plt.ylim(1, 10**5)
 	#plt.ylim(0.5, 1)
