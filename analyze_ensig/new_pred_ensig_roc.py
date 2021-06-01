f5e00a8353a33c9e55b858a2aac05e0689591620
KarenYuanMei <ym2@alum.mit.edu>
2021-06-01 13:38:36 -0700

    draw synsig roc

--- analyze_ensig/new_pred_ensig_roc.py
+++ analyze_ensig/new_pred_ensig_roc.py
@@ -29,19 +29,17 @@ sys.path.append('../run_ML/')
 import predict_ensig_genes
 import compare_controls
 
-def find_nb_all_syngo(go_genes):
-	nb_pool=predict_ensig_genes.find_nonbrain_common_pool()
-	syngo_file='../source_data_files/correct_db/corr_syngo_cc.csv'
-	syngo=load_data_functions.get_gene_names(syngo_file)
-	syngo_nb=list(set(nb_pool)&set(syngo)&set(go_genes))
-	print (len(syngo_nb))
+def find_syngo_nb_big_pool(big_pool, nb_pool):
+	human_ont=find_GO_scores.find_GO_ont()
+	go_genes=human_ont.genes
+	syngo_big_pool=load_data_functions.find_syngo(big_pool, go_genes)
+	syngo_nb=list(set(syngo_big_pool)&set(nb_pool))
+	return syngo_big_pool, syngo_nb
 
-	big_pool_filename='../../../SynSig/synsig_random_forest/big_pool_genes_index.csv'
-	big_pool=load_data_functions.get_gene_names(big_pool_filename)
-	syngo_big_pool=list(set(big_pool)&set(syngo)&set(go_genes))
-	print (len(syngo_big_pool))
-
-	return syngo_nb, syngo_big_pool
+def find_hk_nb(big_pool, nb_pool):
+	hk=compare_controls.find_hk(big_pool)
+	hk_nb=list(set(hk)&set(nb_pool))
+	return hk_nb
 
 def plot_syngo_bargraph(labels, mean_values, xlabel, ylabel, name):
 	x_pos=np.arange(len(labels))
@@ -54,39 +52,21 @@ def plot_syngo_bargraph(labels, mean_values, xlabel, ylabel, name):
 	plt.savefig(name+'.svg', format="svg")
 	plt.close()
 
-if __name__ == '__main__':
-	
-	#find the pool of non-brain genes (genes in all of the nonbrain features)
-	nb_pool=predict_ensig_genes.find_nonbrain_common_pool()
-
-	
-	#find the positive, negative, and all training genes used for predicting ensig:
-	pos, neg, ensig_training=find_training_genes_functions.load_ensig_pos_neg_training()
-
-    #find SynGo genes in the non-brain pool (syngo_nb)
-    #find SynGO genes in big pool (syngo_big_pool)
-	human_ont=find_GO_scores.find_GO_ont()
-	go_genes=human_ont.genes
-	big_pool=load_data_functions.load_big_pool()
-	syngo_big_pool=load_data_functions.find_syngo(big_pool, go_genes)
-	syngo_nb=list(set(syngo_big_pool)&set(nb_pool))
-
-	#find housekeeping genes in the pool of non-brain genes
-	hk=compare_controls.find_hk(big_pool)
-	hk_nb=list(set(hk)&set(nb_pool))
-
-
+def plot_syngo_nb_big_pool_venn(syngo_big_pool, syngo_nb):
 	labels=['All_SynGO', 'Non-Brain SynGO']
 	graph_functions.plot_venn2(syngo_big_pool, syngo_nb, labels, 'nb_vs_all_syngo')
 
+def plot_syngo_nb_big_pool_bargraph(syngo_big_pool, syngo_nb):
 	gene_no=[len(syngo_big_pool), len(syngo_nb)]
 	xlabel='Gene Categories'
 	ylabel='Gene No'
 	plot_syngo_bargraph(labels, gene_no, xlabel, ylabel, 'bar_nb_all_syngo')
 
-
+def plot_nb_ROC_syngo_hk(syngo_nb, hk_nb):
 	ensig_hk_labels=['Non-Brain', 'Housekeeping']
 	ensig_hk=[syngo_nb, hk_nb]
+
+	#load the pred score df from the average ensig predictions:
 	pred_df=load_data_functions.load_predicted_ensig_df()
 
 	for i in range(len(ensig_hk)):
@@ -97,6 +77,35 @@ if __name__ == '__main__':
 		
 		graph_functions.plot_single_ROC(tpr, fpr, auc, ensig_hk_labels[i])
 
+
+
+if __name__ == '__main__':
+	
+	#find the pool of non-brain genes (genes in all of the nonbrain features)
+	nb_pool=predict_ensig_genes.find_nonbrain_common_pool()
+
+	big_pool=load_data_functions.load_big_pool()
+	
+	#find the positive, negative, and all training genes used for predicting ensig:
+	pos, neg, ensig_training=find_training_genes_functions.load_ensig_pos_neg_training()
+
+    #find SynGo genes in the non-brain pool (syngo_nb)
+    #find SynGO genes in big pool (syngo_big_pool)
+    syngo_big_pool, syngo_nb=find_syngo_nb_big_pool(big_pool, nb_pool)
+	
+	#find housekeeping genes in the pool of non-brain genes
+	hk_nb=find_hk_nb(big_pool, nb_pool)
+
+	#compare the syngo genes in all the big pool vs. in the non-brain pool
+	plot_syngo_nb_big_pool_venn(syngo_big_pool, syngo_nb)
+
+	plot_syngo_nb_big_pool_bargraph(syngo_big_pool, syngo_nb)
+
+	#plot the ensig ROC curve for predicting non-brain syngo vs. housekeeping genes
+	plot_nb_ROC_syngo_hk(syngo_nb, hk_nb)
+	
+
+	
 	#graph_functions.plot_annotate_ROC_controls(tpr, fpr, auc)
 
 
