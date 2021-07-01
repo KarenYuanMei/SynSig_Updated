e42a1ca7dad42f7ff59a43095dfdfe999c16a0d1
KarenYuanMei <ym2@alum.mit.edu>
2021-07-01 15:55:36 -0700

    add training gene annotations;

--- analyze_synsig/update_synsig_training_func.py
+++ analyze_synsig/update_synsig_training_func.py
@@ -33,7 +33,7 @@ import find_GO_scores
 import update_synsig_gene_func
 import make_pred_db_data_table
 
-def format_training_ms_count_df(pos_df, all_gl, all_gl_names):
+def format_training_ms_count_df(pos_df, all_gl, all_gl_names, name):
 	pos_genes=pos_df['genes'].tolist()
 	all_counts=[]
 	for item in all_gl:
@@ -51,33 +51,50 @@ def format_training_ms_count_df(pos_df, all_gl, all_gl_names):
 	pos_df['All Sum']=pos_df[all_gl_names].sum(axis=1)
 
 	# count_df['Synapse Percentile']=perc
-	pos_df.to_csv('training_pos_ms_table.csv')
+	pos_df.to_csv('training_%s_ms_table.csv'%name)
 	print (pos_df)
 	return pos_df
 
-training_pos=pd.read_csv('../run_ML/ML_output/training_genes/updated_positives.csv')
-print (training_pos)
-pos_genes=training_pos['genes'].tolist()
+def load_databases():
+	syngo=load_data_functions.find_full_syngo()
+	syndb=load_data_functions.find_full_SynDB()
+	synsysnet=load_data_functions.find_full_synsysnet()
 
-#add description all synsig genes:
-pos_desc=update_synsig_gene_func.add_desc(pos_genes)
-pos_desc.to_csv('training_pos_genes_desc.csv')
+	cortex=load_data_functions.find_full_adult_cortex()
+	striatum=load_data_functions.find_full_adult_striatum()
+	fetal=load_data_functions.find_full_fetal()
+	ngn2=load_data_functions.find_full_ngn2()
+	return syngo, syndb, synsysnet, cortex, striatum, fetal, ngn2
 
-#add mf to all synsig genes:
-pos_desc_mf=update_synsig_gene_func.add_mf_function(pos_genes, pos_desc)
-pos_desc_mf.to_csv('training_pos_desc_mf.csv')
-print (pos_desc_mf)
+def annotate_training_df(pos_genes, name):
 
-syngo=load_data_functions.find_full_syngo()
-syndb=load_data_functions.find_full_SynDB()
-synsysnet=load_data_functions.find_full_synsysnet()
+	#add description all synsig genes:
+	pos_desc=update_synsig_gene_func.add_desc(pos_genes)
+	pos_desc.to_csv('training_%s_genes_desc.csv'%name)
 
-cortex=load_data_functions.find_full_adult_cortex()
-striatum=load_data_functions.find_full_adult_striatum()
-fetal=load_data_functions.find_full_fetal()
-ngn2=load_data_functions.find_full_ngn2()
+	#add mf to all synsig genes:
+	pos_desc_mf=update_synsig_gene_func.add_mf_function(pos_genes, pos_desc)
+	pos_desc_mf.to_csv('training_%s_desc_mf.csv'%name)
+	print (pos_desc_mf)
 
-all_gl=[syngo, syndb, synsysnet, cortex, striatum, fetal, ngn2]
-all_gl_names=['syngo', 'syndb', 'synsysnet', 'cortex', 'striatum', 'fetal', 'ngn2']
+	syngo, syndb, synsysnet, cortex, striatum, fetal, ngn2=load_databases()
 
-format_training_ms_count_df(pos_desc_mf, all_gl, all_gl_names)
+	all_gl=[syngo, syndb, synsysnet, cortex, striatum, fetal, ngn2]
+	all_gl_names=['syngo', 'syndb', 'synsysnet', 'cortex', 'striatum', 'fetal', 'ngn2']
+
+	format_training_ms_count_df(pos_desc_mf, all_gl, all_gl_names, name)
+	
+
+#find training pos genes:
+# training_pos=pd.read_csv('../run_ML/ML_output/training_genes/updated_positives.csv')
+# print (training_pos)
+# pos_genes=training_pos['genes'].tolist()
+# annotate_training_df(pos_genes, 'pos')
+
+#find training negative genes:
+training_neg=pd.read_csv('../run_ML/ML_output/training_genes/updated_negatives.csv')
+print (training_neg)
+neg_genes=training_neg['genes'].tolist()
+annotate_training_df(neg_genes, 'neg')
+annotate_training_df(neg_genes, 'neg')
+
